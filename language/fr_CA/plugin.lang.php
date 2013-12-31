def035f45d5420189570a9b37460b1460d0b9fc8
ddtddt <ddtddt@piwigo.org>
2013-12-31 15:19:00 +0000

    [extensions] - automatic_size - add fr_CA - Quebec - Québécois Thanks to TiOger
    git-svn-id: http://piwigo.org/svn/extensions/automatic_size@26376 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,4 @@
+<?php
+$lang['Automatic'] = 'Automatique';
+$lang['The best adapted size for this photo and your screen is size %s'] = 'La taille la mieux adaptée pour cette photo et votre écran est la taille %s';
+?>

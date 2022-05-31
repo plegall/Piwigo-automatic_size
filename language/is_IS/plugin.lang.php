bf3aca7cf9d72d018f7dad819dd2357d641819b7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-31 07:25:17 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Automatic'] = 'Sjálfvirkt';
+$lang['The best adapted size for this photo and your screen is size %s'] = 'Besta aðlagaða stærðin fyrir þessa mynd og skjáinn þinn er stærðin %s';

a080e0f744be5f4d3e6fe9efcf301db8a56abe7a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:07:26 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Automatic'] = 'אוטומטי';
+$lang['The best adapted size for this photo and your screen is size %s'] = 'הגודל המתאים ביותר לתמונה זו ולמסך שלך הוא גודל %s';

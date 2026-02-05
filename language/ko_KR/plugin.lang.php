5de8d0b6ef7c035390c55afbae6aa02424b2985f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-05 21:34:33 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Automatic'] = '자동';
+$lang['The best adapted size for this photo and your screen is size %s'] = '이 사진과 화면에 가장 적합한 크기는 %s 입니다.';
